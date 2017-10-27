053abf6d1d9297e1e638c36f3ea7a34522bf1a25
anyyanel <31254545+anyyanel@users.noreply.github.com>
2017-10-27 13:54:33 -0500

    Add files via upload
    Programa de prueba

---
+++ programa5.cc
@@ -0,0 +1,17 @@
+//Programa para los procesos de numeros pares y primos
+#include <iostream>
+using namespace std;
+int main()
+{
+  int s=12;
+  int p=2;
+  int flag=0;
+  int divisor=0;
+  cout<<"Inserte un numero p"<<endl; cin>>p;
+  if ((p%2)<=0))
+{p=p+1}
+for(int i=2; i<=p/2; i++)
+  {if(n%i==0){flag=1; divisor=1; break;}}
+if(flag==1;  {p=p+2}
+return 0;}
+
